acefdb36ceaec9284d8519ac66d96d619606b41d
Miandrilala <miandrilala@Miandrilalas-MacBook-Pro.local>
2024-12-06 07:10:05 +0300

    save

--- mz_staydirect/src/StayDirectService.php
+++ mz_staydirect/src/StayDirectService.php
@@ -44,4 +44,33 @@ class StayDirectService {
     return true ;
   }
 
+  function update_username( $current_username, $new_username ) {
+    // Load user by current username.
+    $users = \Drupal::entityTypeManager()
+    ->getStorage( 'user' )
+    ->loadByProperties( [ 'name' => $current_username ] );
+
+    // Ensure user exists.
+    if ( $users ) {
+        $user = reset( $users );
+        // Check if the user is loaded and not anonymous.
+        if ( $user instanceof User && !$user->isAnonymous() ) {
+            // Update the username.
+            $user->setUsername( $new_username );
+
+            // Save the updated user.
+            try {
+                $user->save();
+                \Drupal::logger( 'mz_staydirect' )->info( 'Username updated successfully.' );
+            } catch ( \Exception $e ) {
+                \Drupal::logger( 'mz_staydirect' )->error( 'Failed to update username: ' . $e->getMessage() );
+            }
+        } else {
+            \Drupal::logger( 'mz_staydirect' )->warning( 'User could not be found or is anonymous.' );
+        }
+    } else {
+        \Drupal::logger( 'mz_staydirect' )->warning( 'No user found with the given username.' );
+    }
+}
+
 }
