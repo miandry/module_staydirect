fbd235e0c1b764ed3d238c4df3aedac2dff5a55a
root <root@ubuntu-cloudpanel.localhost>
2025-01-16 05:13:42 +0000

    save

--- mz_staydirect/src/Form/unsubscribeForm.php
+++ mz_staydirect/src/Form/unsubscribeForm.php
@@ -10,9 +10,11 @@ use Drupal\Core\Url;
 use Drupal\file\Entity\File;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
+
 /**
  * Edit config variable form.
  */
+ 
 class unsubscribeForm extends FormBase
 {
 
@@ -29,35 +31,26 @@ class unsubscribeForm extends FormBase
    */
   public function buildForm(array $form, FormStateInterface $form_state, $config_name = '')
   { $id = \Drupal::request()->query->get('id');
-    \Drupal::logger('mz_staydirect')->notice('Ceci est un message d\'information.');
-
+ 
     if($id == null ){
       $base_url = \Drupal::request()->getSchemeAndHttpHost();
       $url =    $base_url.'/user';
       $response = new RedirectResponse($url);
       $response->send();
     }
+    $form['#theme'] = 'unsubscribe_theme';
     $form['id'] = [
         '#type' => 'hidden',
         '#value' => $id,
     ];
-    $body =  $this->include_template("unsubscribe-content.html.twig",[]) ;
-    $form['text_unsubscribe_1'] = [
-      '#markup' => '<div class="unsubscribe_content container"><div class="row"><div class="col">',
-    ];
-    $form['text_unsubscribe_2'] = [
-      '#markup' => $body,
-    ];
-      $form['submit'] = [
+    $form['submit'] = [
         '#type' => 'submit',
         '#value' => $this->t('Unsubscribe'),
         '#attributes' => [
-          'class' => ['btn-primary'],
+          'class' => ['btn-custom'],
         ],
-      ];
-      $form['text_unsubscribe_3'] = [
-        '#markup' =>'</div></div></div>',
-      ];
+    ];
+ 
     return $form;
   }
 
@@ -78,7 +71,6 @@ class unsubscribeForm extends FormBase
     if(isset($values["id"])){
       $id = $values["id"];
       $site = \Drupal::entityTypeManager()->getStorage('node')->load($id);
-      kint($site);die();
       $message = 'Failed to unSubscribe';
       $status = false ;
       if(is_object($site)){
@@ -89,26 +81,20 @@ class unsubscribeForm extends FormBase
         if($current_user_id === $node_author_id || 
           in_array('admin',  $roles) || 
           in_array('webmaster',  $roles)) {
-
-          $subscriptionId = $params_site['subscriptionId'];
           $service = \Drupal::service('mz_payment.manager');
-          $result = $service->unSubscription($subscriptionId);
+          $result = $service->executeUnSubscription($site);
           if($result){
-            $message = 'You have sucessfully unSubscribe in STRIPE with id='.$subscriptionId;
-            \Drupal::logger('mz_staydirect')->notice($message);
-              
-
-            // $base_url = \Drupal::request()->getSchemeAndHttpHost();
-            // $url =    $base_url.'/user';
-            // $response = new RedirectResponse($url);
-            // $response->send();
-
+            $message = 'You have sucessfully unSubscribe ';
+            \Drupal::messenger()->addMessage($message);
           }else{
             $message = 'Failed unSubscribe in STRIPE with id='.$subscriptionId;
             \Drupal::logger('mz_staydirect')->error($message);
           }
+          $base_url = \Drupal::request()->getSchemeAndHttpHost();
+          $url =    $base_url.'/user';
+          $response = new RedirectResponse($url);
+          $response->send();
       
-
         } else {
 
           $message = 'You dont have permission  to unSubscribe !! , please the website admin';
